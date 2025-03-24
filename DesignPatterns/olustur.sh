967d595f35c8b29aa286e7c9e67476a306e6702f
Tetsuya <oyleiste0f@hotmail.com>
2025-03-25 01:13:33 +0300

    Add dirs for patterns

---
+++ DesignPatterns/olustur.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+mkdir -p DesignPatterns
+
+mkdir -p DesignPatterns/Creational
+mkdir -p DesignPatterns/Creational/Factory
+mkdir -p DesignPatterns/Creational/Factory/FactoryMethod
+mkdir -p DesignPatterns/Creational/Factory/AbstractFactory
+mkdir -p DesignPatterns/Creational/Factory/StaticFactoryMethod
+mkdir -p DesignPatterns/Creational/Factory/StaticNestedFactory
+mkdir -p DesignPatterns/Creational/Factory/SimpleFactory
+mkdir -p DesignPatterns/Creational/Singleton
+mkdir -p DesignPatterns/Creational/Builder
+mkdir -p DesignPatterns/Creational/Prototype
+mkdir -p DesignPatterns/Creational/ObjectPool
+mkdir -p DesignPatterns/Creational/DependencyInjection
+mkdir -p DesignPatterns/Creational/LazyInitialization
+
+mkdir -p DesignPatterns/Structural
+mkdir -p DesignPatterns/Structural/Adapter
+mkdir -p DesignPatterns/Structural/Bridge
+mkdir -p DesignPatterns/Structural/Composite
+mkdir -p DesignPatterns/Structural/Decorator
+mkdir -p DesignPatterns/Structural/Facade
+mkdir -p DesignPatterns/Structural/Flyweight
+mkdir -p DesignPatterns/Structural/Proxy
+mkdir -p DesignPatterns/Structural/PrivateClassData
+mkdir -p DesignPatterns/Structural/Module
+
+mkdir -p DesignPatterns/Behavioral
+mkdir -p DesignPatterns/Behavioral/CommandGroup
+mkdir -p DesignPatterns/Behavioral/CommandGroup/Command
+mkdir -p DesignPatterns/Behavioral/CommandGroup/CommandProcessor
+mkdir -p DesignPatterns/Behavioral/Interpreter
+mkdir -p DesignPatterns/Behavioral/Iterator
+mkdir -p DesignPatterns/Behavioral/Mediator
+mkdir -p DesignPatterns/Behavioral/Memento
+mkdir -p DesignPatterns/Behavioral/Observer
+mkdir -p DesignPatterns/Behavioral/State
+mkdir -p DesignPatterns/Behavioral/Strategy
+mkdir -p DesignPatterns/Behavioral/TemplateMethod
+mkdir -p DesignPatterns/Behavioral/Visitor
+mkdir -p DesignPatterns/Behavioral/ChainOfResponsibility
+mkdir -p DesignPatterns/Behavioral/NullObject
+mkdir -p DesignPatterns/Behavioral/Specification
+
+mkdir -p DesignPatterns/Concurrency
+mkdir -p DesignPatterns/Concurrency/ActiveObject
+mkdir -p DesignPatterns/Concurrency/MonitorObject
+mkdir -p DesignPatterns/Concurrency/ThreadPool
+mkdir -p DesignPatterns/Concurrency/Balking
+mkdir -p DesignPatterns/Concurrency/DoubleCheckedLocking
+mkdir -p DesignPatterns/Concurrency/LeaderFollowers
+mkdir -p DesignPatterns/Concurrency/ReadWriteLock
+mkdir -p DesignPatterns/Concurrency/Scheduler
+mkdir -p DesignPatterns/Concurrency/ThreadLocalStorage
+
+mkdir -p DesignPatterns/Architectural
+mkdir -p DesignPatterns/Architectural/MVC
+mkdir -p DesignPatterns/Architectural/MVP
+mkdir -p DesignPatterns/Architectural/MVVM
+mkdir -p DesignPatterns/Architectural/Repository
+mkdir -p DesignPatterns/Architectural/ServiceLocator
+mkdir -p DesignPatterns/Architectural/LayeredArchitecture
+mkdir -p DesignPatterns/Architectural/Microservices
+mkdir -p DesignPatterns/Architectural/PipesAndFilters
+mkdir -p DesignPatterns/Architectural/Blackboard
+mkdir -p DesignPatterns/Architectural/EventDriven
+
+echo "Tamamlandi"
